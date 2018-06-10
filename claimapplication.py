3d0f59e012b138e874965d6700f0fa319b122813
eswaribala <Parameswaribala@gmail.com>
2018-06-10 12:46:52 +0530

    Add files via upload

--- claimapplication.py
+++ claimapplication.py
@@ -6,16 +6,22 @@ Created on Sun Jun 10 09:35:56 2018
 """
 from policy_holder import PolicyHolder
 from driver import Driver
-from vehicle import Vehicle
+from vehicle import Vehicle,HighEndVehicle,LowEndVehicle
 import datetime
 #create object
 fromDate = datetime.date(2015,1, 1)  #year, month, day
 toDate=datetime.date(2019,1,1)
 dob=datetime.date(1978,4,21)
 
-vehicleObj=Vehicle("TN-02-4556",43876587435,"Red")
+vehicleObj=HighEndVehicle("TN-02-4556",43876587435,"Red",True,True,True)
 vehicleObj.setPolicyHolder(PolicyHolder(237467,fromDate,toDate,"Sanjay","Male",dob))
 vehicleObj.setDriver(Driver(429654,"Kiran"))
 
 print(vehicleObj.getPolicyHolder().getName(),"--->",vehicleObj.getRegNo())
+print(Vehicle.baseRoadTax)
 
+print(vehicleObj.computeTaxByVehicle(2))
+
+vehicleObj=LowEndVehicle("TN-02-4556",43876587435,"Red",True)
+
+print(vehicleObj.computeTaxByVehicle(4))
--- vehicle.py
+++ vehicle.py
@@ -6,6 +6,8 @@ Created on Sun Jun 10 11:45:06 2018
 """
 
 class Vehicle:
+    #static variable 
+    baseRoadTax=5000
     def __init__(self,regNo,engineNo,color):
         self.__regNo=regNo
         self.__engineNo=engineNo
@@ -28,3 +30,37 @@ class Vehicle:
     
     def getDriver(self):
         return self.__driverObj
+    
+    @staticmethod
+    def computeTax(wheels):
+        if (wheels==2):
+            return Vehicle.baseRoadTax+2000
+        if(wheels==4):
+             return Vehicle.baseRoadTax+4000
+         
+    @classmethod
+    def computeTaxByVehicle(cls,wheels):
+        if ((wheels==2) and (cls.__name__=="HighEndVehicle")):
+            return Vehicle.baseRoadTax+2000+Vehicle.baseRoadTax * .6
+        if ((wheels==4) and (cls.__name__=="LowEndVehicle")):
+             return Vehicle.baseRoadTax+4000++Vehicle.baseRoadTax * .4
+         
+            
+#inheritance
+class HighEndVehicle(Vehicle):
+    
+    def __init__(self,regNo,engineNo,color,airbrake,gps,autogear):
+        Vehicle.__init__(self,regNo,engineNo,color) #invoke super class
+        self.__abs=airbrake
+        self.__gps=gps
+        self.autogear=autogear
+        
+class LowEndVehicle(Vehicle):
+    
+    def __init__(self,regNo,engineNo,color,childLock):
+        Vehicle.__init__(self,regNo,engineNo,color) #invoke super class
+        self.__childLock=childLock
+        
+        
+    
+    
