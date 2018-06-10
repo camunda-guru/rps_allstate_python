c6a737185cf8e6c21ab58fd76a2bc005f8c50c5d
eswaribala <Parameswaribala@gmail.com>
2018-06-10 17:12:50 +0530

    Add files via upload

--- DbConnectionDemo.py
+++ DbConnectionDemo.py
@@ -12,7 +12,8 @@ def createConnection():
                       'SERVER=DESKTOP-55AGI0I\SQLEXPRESS;DATABASE=AllStateDB;'
                       'UID=sa;PWD=vignesh')
     return conn;
-
+#SELECT p.PolicyNo,c.CustomerName,p.MaturityAmount
+#  FROM dbo.Policy p, dbo.Customer c where p.PolicyNo=c.PolicyNo;
 
 def addPolicy(data):
     connObj = createConnection();
@@ -26,8 +27,32 @@ def addPolicy(data):
     except pyodbc.Error as e:
         print("Exception occurred", e)
         connObj.rollback()
-        
 
-testData=[4222,2453453,4274];
-addPolicy(testData);        
+def getPolicies():
+    connObj = createConnection()
+    cursor = connObj.cursor()
+    cursor.execute("select * from Policy")
+    return cursor.fetchall()
+      
+def fetchByPolicyId(id):
+    conn = createConnection()
+    cursor = conn.cursor()
+    cursor.execute("""select * from Policy where 
+    PolicyNo='%d'""" % (id))
+    return cursor.fetchall()
+
+def fetchByPolicyandCustomerById():
+    conn = createConnection()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT p.PolicyNo,c.CustomerName,p.MaturityAmount
+  FROM Policy p, Customer c where 
+     p.PolicyNo=c.PolicyNo""" )
+    return cursor.fetchall()
+
+#testData=[4222,2453453,4274];
+#addPolicy(testData); 
+#Retrieve Data
+for row in fetchByPolicyandCustomerById():
+    print(row)
+       
         
