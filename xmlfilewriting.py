0d0a1b1e370ba694e499772995a914e3c53b0e2e
eswaribala <Parameswaribala@gmail.com>
2018-06-23 14:52:02 +0530

    Add files via upload

--- xmlfilewriting.py
+++ xmlfilewriting.py
@@ -11,38 +11,40 @@ doc=minidom.Document()
 #rootnode
 rootElement=doc.createElement("Claims")
 doc.appendChild(rootElement)
-#claim
-node=doc.createElement("Claim")
-#node.setAttribute('ClaimId','12345')
-rootElement.appendChild(node)
-#claim id Node
-childNode=doc.createElement("ClaimId")
-textNode = doc.createTextNode('1234')
-childNode.appendChild(textNode)
-node.appendChild(childNode)
 
-#PolicyNo Node
-childNode=doc.createElement("PolicyNo")
-textNode = doc.createTextNode('244354')
-childNode.appendChild(textNode)
-node.appendChild(childNode)
+file =open("claimdata.csv","r")
 
-#Claim Amount
-childNode=doc.createElement("ClaimAmount")
-textNode = doc.createTextNode('58000')
-childNode.appendChild(textNode)
-node.appendChild(childNode)
-#Status
-childNode=doc.createElement("Status")
-textNode = doc.createTextNode('In Progress')
-childNode.appendChild(textNode)
-node.appendChild(childNode)
-
-#clement
-childNode=doc.createElement("Clement")
-textNode = doc.createTextNode('Raghu')
-childNode.appendChild(textNode)
-node.appendChild(childNode)
+for line in file:   
+    temp=line.split(",")
+    if(temp[0] !="ClaimId"):
+        #claim
+        node=doc.createElement("Claim")
+        #node.setAttribute('ClaimId','12345')
+        rootElement.appendChild(node)
+        #claim id Node
+        childNode=doc.createElement("ClaimId")
+        textNode = doc.createTextNode(temp[0])
+        childNode.appendChild(textNode)
+        node.appendChild(childNode)
+        
+        #PolicyNo Node
+        childNode=doc.createElement("PolicyNo")
+        textNode = doc.createTextNode(temp[1])
+        childNode.appendChild(textNode)
+        node.appendChild(childNode)
+        
+        #Claim Amount
+        childNode=doc.createElement("ClaimAmount")
+        textNode = doc.createTextNode(temp[2])
+        childNode.appendChild(textNode)
+        node.appendChild(childNode)
+        #Status
+        childNode=doc.createElement("Status")
+        textNode = doc.createTextNode(temp[3])
+        childNode.appendChild(textNode)
+        node.appendChild(childNode)
+    
+    
 #indentation
 xml_str = doc.toprettyxml(indent="  ")
 with open("allstate_claims.xml", "w") as f:
